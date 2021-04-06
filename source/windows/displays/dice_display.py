c5df88271125dd54bbb8660d07ecaff252842c11
Franco Curotto <francocurotto@gmail.com>
2021-04-06 21:30:22 +0900

    solve the emoji issue by ussing special emojis with fixed width

--- source/windows/displays/dice_display.py
+++ source/windows/displays/dice_display.py
@@ -23,24 +23,36 @@ def create_widget_list(dice):
     # name widget
     widget_list.append(urwid.Text(card.name, wrap="clip"))
 
-    # string for the next widget
-    string = icons["TYPE_"+card.type]+str(card.level) + " "
+    # type widget
+    string = icons["TYPE_"+card.type]+str(card.level)
+    widget_list.append(("pack",urwid.Text(string)))
 
-    # monster specific string part
+    # monster specific widget
     if dice.card.is_monster():
-        # attack part
-        string += str(card.attack).rjust(2)
-        string += icons["CREST_ATTACK"] + " "
+        # attack widget
+        string  = str(card.attack).rjust(2)
+        string += icons["CREST_ATTACK"]
+        widget_list.append(("pack",urwid.Text(string)))
         # defense widget
-        string += str(card.defense).rjust(2)
-        string += icons["CREST_DEFENSE"] + " "
+        string  = str(card.defense).rjust(2)
+        string += icons["CREST_DEFENSE"]
+        widget_list.append(("pack",urwid.Text(string)))
         # life widget
-        string += str(card.life).rjust(2)
+        string  = str(card.life).rjust(2)
         string += icons["MONSTER_HEART"]
+        widget_list.append(("pack",urwid.Text(string)))
     
     # in case of item add a blank section for alignment
     elif dice.card.is_item():
-        string += " "*11
+        dummy_str = "AA" + icons["CREST_ATTACK"] + " " + \
+                    "DD" + icons["CREST_DEFENSE"] + " " + \
+                    "LL" + icons["MONSTER_HEART"]
+        dummy_text = urwid.Text(dummy_str)
+        width = dummy_text.pack()[0]
+        string = width*" "
+        widget_list.append(("pack",urwid.Text(string)))
+            
+
 
     # dice widget
     dice_str_list = []
@@ -48,8 +60,7 @@ def create_widget_list(dice):
         dice_str  = icons["CREST_"+side.crest.type]
         dice_str += str(side.multiplier)
         dice_str_list.append(dice_str)
-    string += " " + ",".join(dice_str_list)
-
+    string = ",".join(dice_str_list)
     widget_list.append(("pack",urwid.Text(string)))
 
     return widget_list
