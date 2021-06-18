dadb111ea07f1538cef670a2296563e4e7e7223f
Franco Curotto <francocurotto@gmail.com>
2021-06-17 20:49:04 -0400

    moving files

---
+++ source/clients/urwid/globvars.py
@@ -0,0 +1,16 @@
+# library vars
+starter_pool_path = "clients/urwid/pools/starter.yaml"
+saved_pool_path   = "clients/urwid/pools/saved.yaml"
+
+# settings vars
+default_settings_path = "clients/urwid/settings/default.yaml"
+saved_settings_path   = "clients/urwid/settings/saved.yaml"
+
+# dice info vars
+# icons var
+import yaml
+icons = yaml.full_load(open("clients/ICONS.yaml"))
+
+# urwid pallete
+palette = [("focused", "standout", "")]
+
